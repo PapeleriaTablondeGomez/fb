b74052cf56ed853e1f78ae4fc67d7174bee2563d
termuxbyte <50546720+termuxbyte@users.noreply.github.com>
2023-08-25 14:43:13 -0500

    Update fb.php

--- fb.php
+++ fb.php
@@ -11,25 +11,24 @@ function curl($url) {
 
 $defaultSavePath = '/ruta/completa/por/defecto/';
 
-echo "TermuxByte2022\n\n";
+system("clear");
+echo "                                                                                                    
+TermuxByte2022
+
+";
+echo "\n\n";
 echo "[#] Ingrese la URL del Video Ej: (https://www.facebook.com/user/video/id) : ";
 $v = trim(fgets(STDIN, 1024));
 echo "\n\n[#] Nombre para el video : ";
 $name = trim(fgets(STDIN, 1024));
-
 $url = str_replace('www', 'mbasic', $v);
 $s = curl($url);
-
-if (preg_match('/"videoData".*?"src":"(.*?)"/', $s, $matches)) {
-    $vurl = json_decode('"' . $matches[1] . '"');
-    
-    echo "\n\n[+] Descargando... \n\n\n";
-    $saveFilePath = $defaultSavePath . $name . '.mp4';
-    $d = 'wget -O "' . $saveFilePath . '" --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6" "' . $vurl . '" -q --show-progress';
-
-    system($d);
-    echo "\n\n[+] Listo.. Video Descargado en: " . $saveFilePath . "\n\n";
-} else {
-    echo "No se pudo encontrar la URL del video.";
-}
+//echo $s;
+$vurl = preg_match('/<a href=\"\/video_redirect\/\?src\=(.*?)\"/ims', $s, $matches) ? $matches[1] : null;
+$vu = urldecode($vurl);
+echo "\n\n[+] Descargando... \n\n\n";
+$d = 'wget -O "' . $defaultSavePath . $name . '.mp4" --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6" "' . $vu . '" -q --show-progress';
+system($d);
+echo "\n\n[+] Listo.. Video Descargado : " . $defaultSavePath . $name . ".mp4\n\n";
+exit(0);
 ?>
